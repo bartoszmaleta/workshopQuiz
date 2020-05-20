0b49898c9838625f2421bbd32aa847484c853b4e
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-05-20 11:07:13 +0200

    Initial commit

---
+++ Public/js/script.js
@@ -0,0 +1,9 @@
+data = {
+    "quizcontent": [
+        { "question": "question text 1", "correct": 1, "a1": "answer 1", "a2": "answer 2", "a3": "answer 3", "a4": "answer 4" },
+        { "question": "question text 2", "correct": 3, "a1": "answer 1", "a2": "answer 2", "a3": "answer 3", "a4": "answer 4" },
+        { "question": "question text 3", "correct": 4, "a1": "answer 1", "a2": "answer 2", "a3": "answer 3", "a4": "answer 4" },
+        { "question": "question text 4", "correct": 4, "a1": "answer 1", "a2": "answer 2", "a3": "answer 3", "a4": "answer 4" }
+    ],
+};
+
