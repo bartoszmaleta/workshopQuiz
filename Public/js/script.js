354dcc33b1635ed2aa844d2fe3c45998be86e966
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-05-20 11:10:37 +0200

    Change first example  question

--- Public/js/script.js
+++ Public/js/script.js
@@ -1,6 +1,6 @@
 data = {
     "quizcontent": [
-        { "question": "question text 1", "correct": 1, "a1": "answer 1", "a2": "answer 2", "a3": "answer 3", "a4": "answer 4" },
+        { "question": "What 'git status' do?", "correct": 1, "a1": "Show branches", "a2": "Show status of git tree", "a3": "Push changes", "a4": "Commit changes" },
         { "question": "question text 2", "correct": 3, "a1": "answer 1", "a2": "answer 2", "a3": "answer 3", "a4": "answer 4" },
         { "question": "question text 3", "correct": 4, "a1": "answer 1", "a2": "answer 2", "a3": "answer 3", "a4": "answer 4" },
         { "question": "question text 4", "correct": 4, "a1": "answer 1", "a2": "answer 2", "a3": "answer 3", "a4": "answer 4" }
